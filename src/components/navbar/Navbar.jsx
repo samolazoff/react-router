86e9b553443edb661c53827079596adc9beb2081
unknown <samolazov.roman.alex@gmail.com>
2023-06-04 14:16:09 +0300

    Work with style of active NAvLink

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,14 +3,15 @@ import {NavLink} from 'react-router-dom';
 
 import './navbar.css';
 
+const setActive = ({isActive}) => isActive ? 'active-item': '';
 const Navbar = () => {
     return (
         <nav className='navbar cont f-r'>
             <h1 className='logo'><a href="/"> Router</a></h1>
             <ul className='navbar-items f-r'>
-                <li className='item'><NavLink to="/">Home</NavLink></li>
-                <li className='item'><NavLink to="/posts">Posts</NavLink></li>
-                <li className='item'><NavLink to="/about">About</NavLink></li>
+                <li className='item'><NavLink to="/" className={setActive}>Home</NavLink></li>
+                <li className='item'><NavLink to="/posts" className={setActive}>Posts</NavLink></li>
+                <li className='item'><NavLink to="/about" className={setActive}>About</NavLink></li>
             </ul>
         </nav>
     );
